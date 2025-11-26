a30fac6b5009df5e7b0be3e482ba891338743d8e
RChefo <rchefocontact.com@gmail.com>
2025-11-26 11:50:17 +0200

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'], // البورت اللي React app شغالة عليه
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
